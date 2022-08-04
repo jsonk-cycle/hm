4c83033b693c512b84b0f89b576a8f2c2bc158df
jsonk-cycle <226164639@qq.com>
2022-08-04 14:15:22 +0800

    移动端项目的创建和初始化以及eslint的配置

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+<!-- git init形成本地仓库 -->
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 浏览器报错消除
 Vue.config.productionTip = false
 
 new Vue({
@@ -10,3 +11,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+// webpack开发服务器在下面终端中+eslit插件,vscode+eslint插件+目录下eslinrc.js配置,两处检测代码
